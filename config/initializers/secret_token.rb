4490ba08ff109b4bdee39f21455790338c7ac5dc
Dylan Barth <dlnbarth@gmail.com>
2012-04-11 16:27:42 -0700

    Royalty reporting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AffablRoyalty::Application.config.secret_token = 'fd16df12b55525a0c02b724256d965a2cfea84f7d7fd7a43ee4318b9159b8d4ac3075b4e96ced1783a08e81e89a3c8eb676952e04ffe15db23a72525f936e98e'
